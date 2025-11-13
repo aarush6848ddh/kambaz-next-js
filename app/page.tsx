624ab5f400cea04cb426c3b92104fc157f608c68
Aarush Singh <106162023+aarush6848ddh@users.noreply.github.com>
2025-11-12 22:19:53 -0500

    Fix root page redirect using client-side navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,19 @@
 "use client";
 
-import { Provider } from "react-redux";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import store from "./(Kambaz)/store";
-import Session from "./(Kambaz)/Account/Session";
 
 export default function Home() {
   const router = useRouter();
   
   useEffect(() => {
-    router.replace("/Account");
+    router.push("/Account");
   }, [router]);
   
   return (
-    <Provider store={store}>
-      <Session>
-        <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
-          <p>Redirecting...</p>
-        </div>
-      </Session>
-    </Provider>
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+      <p>Loading...</p>
+    </div>
   );
 }
 
