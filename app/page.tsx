45fb86daae36b83210e9d49e8272b48cff077c00
Aarush Singh <106162023+aarush6848ddh@users.noreply.github.com>
2025-11-12 21:44:07 -0500

    Wrap root page with Redux Provider and Session for redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,26 @@
 "use client";
 
-import AccountRedirect from "./(Kambaz)/Account/AccountRedirect";
+import { Provider } from "react-redux";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import store from "./(Kambaz)/store";
+import Session from "./(Kambaz)/Account/Session";
 
 export default function Home() {
-  return <AccountRedirect />;
+  const router = useRouter();
+  
+  useEffect(() => {
+    router.replace("/Account");
+  }, [router]);
+  
+  return (
+    <Provider store={store}>
+      <Session>
+        <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+          <p>Redirecting...</p>
+        </div>
+      </Session>
+    </Provider>
+  );
 }
 
